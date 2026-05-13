1424b77b0998452ab72937114d6682a523ccd5ab
Giovanna <giovannamartinin@MacBook-Air-de-Giovanna.local>
2026-05-13 20:26:05 -0300

    commit inicial com esqueleto

---
+++ src/main/java/com/javanauta/agendador/tarefas/AgendadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.agendador.tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorTarefasApplication.class, args);
+	}
+
+}
