85e03e553b856036c46e2386c3222234d4818bc0
Giovanna <giovannamartinin@MacBook-Air-de-Giovanna.local>
2026-05-23 14:02:04 -0300

    adicionado metodo post

--- src/main/java/com/javanauta/agendador/tarefas/business/TarefasService.java
+++ src/main/java/com/javanauta/agendador/tarefas/business/TarefasService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -30,4 +31,9 @@ public class TarefasService {
         return tarefaConverter.paraTarefaDTO(
                 tarefasRepository.save(entity));
     }
+
+    public List<TarefasDTO> buscaTarefasAgendadasPorPeriodo(LocalDateTime dataInicial,LocalDateTime dataFinal){
+
+    }
+
 }
--- src/main/java/com/javanauta/agendador/tarefas/infrastructure/repository/TarefasRepository.java
+++ src/main/java/com/javanauta/agendador/tarefas/infrastructure/repository/TarefasRepository.java
@@ -4,6 +4,10 @@ import com.javanauta.agendador.tarefas.infrastructure.entity.TarefasEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface TarefasRepository extends MongoRepository<TarefasEntity, String> {
+    List<TarefasEntity> findByDataEventoBetween(LocalDateTime dataInicial, LocalDateTime dataFinal);
 }
